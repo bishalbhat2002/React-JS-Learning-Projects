0d94505fc0fe9253032c799658ba8c6a82fadda0
Bishal Bhat <bishalbhat2002@gmail.com>
2025-09-02 10:10:19 +0545

    Minor CSS classes Updated

--- src/components/CartContext/Cart.jsx
+++ src/components/CartContext/Cart.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import { items } from "./items";
 import { CartContext } from "./CartContext";
+import Header from "../SmallComponents/Header"
+import Explanation from "../SmallComponents/Explanation"
 
 const Cart = () => {
   const { cartItems, cartPrice, setCartItems, setCartPrice, showItems, setShowItems} =
@@ -12,26 +14,23 @@ const Cart = () => {
      setCartPrice(cartPrice + items[index].price)
      setShowItems(true);
     }
+    
+    const explained = "here,.";
   return (
     <>
-      <div className="w-full bottom-10 flex justify-center">
-        <h1 className="my-2 px-10 py-4 rounded-md text-lg xl:text-2xl font-extrabold bg-blue-400 text-gray-800 inline-block">
-          Add to Cart Feature Using UseContext
-        </h1>
-      </div>
+      <Header heading="Add to Cart Feature Using UseContext" />
 
       {/* Data Show To add Cart */}
-
       <table className="border mx-auto">
         <thead className="border">
           <tr>
-            <th className="border px-8 py-2 bg-yellow-200 text-gray-800">
+            <th className="border px-2 py-1 text-md bg-yellow-200 text-gray-800">
               Name
             </th>
-            <th className="border px-8 py-2 bg-yellow-200 text-gray-800">
+            <th className="border px-2 py-1 text-md bg-yellow-200 text-gray-800">
               Price
             </th>
-            <th className="border px-8 py-2 bg-yellow-200 text-gray-800">
+            <th className="border px-2 py-1 text-md bg-yellow-200 text-gray-800">
               Action
             </th>
           </tr>
@@ -40,12 +39,12 @@ const Cart = () => {
           {items.map((item, index) => {
             return (
               <tr key={index}>
-                <td className="border px-8 py-2">{item.name}</td>
-                <td className="border px-5 py-2">{item.price}</td>
-                <td className="border px-8 py-2">
+                <td className="border px-2 py-1 text-md">{item.name}</td>
+                <td className="border px-2 py-1 text-md">{item.price}</td>
+                <td className="border px-2 py-1 text-md">
                   <button
                   onClick={()=>handleAddToCartBtn(index)} 
-                  className="bg-blue-300 px-4 py-1 rounded-md hover:scale-110 active:scale-95 transition-transform duration-200 ease-in-out">
+                  className="bg-blue-300 text-gray-700 px-4 py-1 text-sm font-semibold rounded-md hover:scale-110 active:scale-95 transition-transform duration-200 ease-in-out">
                     Add To Cart
                   </button>
                 </td>
@@ -66,14 +65,14 @@ export default Cart;
 function CartDetails() {
        const { cartItems, cartPrice, showItems} = useContext(CartContext);
   return (
-    <section className="mx-auto mt-5 w-100 lg:w-120 my-2 px-10 py-4 rounded-md  bg-blue-200">
+    <section className="mx-auto mt-5 w-90 lg:w-160 my-2 px-10 py-4 rounded-md  bg-blue-200">
       <span className="text-lg xl:text-2xl font-extrabold  text-gray-800">
         Total: <span className="font-bold text-gray-700"> {cartPrice} RS</span>
       </span>
       {
           showItems ? 
-          <div className="-mx-5 bg-gray-400 py-4 px-5 rounded-lg">
-          <ul>
+          <div className="-mx-5 bg-gray-300 border border-gray-500 py-4 px-5 rounded-lg">
+          <ul className="text-gray-700">
                {
                     cartItems.map((item, index)=>{
                          return (
