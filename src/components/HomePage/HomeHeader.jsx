4b37261c62e25baf96f56c441b75f406ec2f3e5c
Bishal Bhat <bishalbhat2002@gmail.com>
2025-09-02 18:35:49 +0545

    Minor CSS Classes Updated For Responsive Design

--- src/components/HomePage/HomeHeader.jsx
+++ src/components/HomePage/HomeHeader.jsx
@@ -1,11 +1,12 @@
 
+
 const Header = () => {
   return (
     <>
      {/* Heading Section */}
       <section className="flex justify-center">
         <h1 className="inline-block mt-3 p-5 lg:p-7 rounded-md lg:text-2xl font-extrabold bg-amber-400 text-gray-700">
-          25 Basic Projects while Learning React
+          Some Basic Projects while Learning React
         </h1>
       </section>
     </>
--- src/components/HomePage/ShowProjects.jsx
+++ src/components/HomePage/ShowProjects.jsx
@@ -7,30 +7,46 @@ import { Routes, Route, Link } from "react-router";
 import { GoArrowUpRight } from "react-icons/go";
 import Header from "./HomeHeader";
 
-const ShowProjects = () => {
 
+const ShowProjects = () => {
   return (
     <>
-
       <Header />
-      
-    {/* Project Show Section */}
-      <section className="container mx-auto flex justify-center flex-wrap gap-3 md:gap-7 mt-4 p-3 md:p-7 rounded-md bg-blue-200 text-gray-700">
+
+      {/* Project Show Section */}
+      <section className="w-90 lg:w-9/10  mx-auto mb-5 flex justify-center flex-wrap gap-3 lg:gap-6 mt-4 p-5 lg:p-7 rounded-md bg-blue-200 text-gray-700">
         {components.map((component, index) => {
-          return <div key={index} className="max-h-50 w-100 p-5 pb-3 md:p-5 rounded bg-white">
-               
-               <h2 className="text-gray-600 text-lg md:text-2xl font-semibold">{index+1}.&nbsp;{component.projectName}</h2>
+          return (
+            <div
+              key={index}
+              className="max-h-50 w-100 p-5 pb-3 md:p-5 rounded bg-white"
+            >
+              <h2 className="text-gray-600 text-lg md:text-2xl font-semibold">
+                {index + 1}.&nbsp;{component.projectName}
+              </h2>
 
-               <p className="mt-1 ml-5 text-md md:text-md text-gray-500">{component.description}</p>
+              <p className="mt-1 ml-5 text-md md:text-md text-gray-500">
+                {component.description}
+              </p>
 
-               <div className="flex justify-end">
-               <Link to={component.path} className="mt-2 md:mt-5 px-2 md:px-4 py-1 rounded-lg text;md md:text-lg bg-blue-400 text-white hover:scale-110 transition-transform duration-200 ease-in-out active:scale-90 opacity-80 hover:opacity-100">
-                         View Project <GoArrowUpRight className="inline-block" /> 
-               </Link>
-               </div>
-          </div>;
+              <div className="flex justify-end">
+                <Link
+                  to={component.path}
+                  className="mt-2 md:mt-5 px-2 md:px-4 py-1 rounded-lg text;md md:text-lg bg-blue-400 text-white hover:scale-110 transition-transform duration-200 ease-in-out active:scale-90 opacity-80 hover:opacity-100"
+                >
+                  View Project <GoArrowUpRight className="inline-block" />
+                </Link>
+              </div>
+            </div>
+          );
         })}
       </section>
+      <div className="text-center mb-5">
+        <a href="https://github.com/bishalbhat2002/React-JS-Learning-Projects" target="_blank"
+        className="inline-block px-5 py-2 rounded-md font-semibold text-gray-100 bg-blue-400 hover:scale-110 transition-transform duration-200 ease-in-out">
+          View-Code
+        </a>
+      </div>
     </>
   );
 };
