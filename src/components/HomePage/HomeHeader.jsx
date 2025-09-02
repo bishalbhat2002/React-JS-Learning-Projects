89e40c176ac9e099537f760a5b023c635827d492
Bishal Bhat <bishalbhat2002@gmail.com>
2025-09-02 11:23:48 +0545

    Minor CSS classes change

--- src/components/HomePage/HomeHeader.jsx
+++ src/components/HomePage/HomeHeader.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     <>
      {/* Heading Section */}
       <section className="flex justify-center">
-        <h1 className="inline-block mt-4 p-5 lg:m-6 lg:p-7 rounded-md lg:text-4xl font-extrabold bg-amber-400 text-gray-700">
+        <h1 className="inline-block mt-3 p-5 lg:p-7 rounded-md lg:text-2xl font-extrabold bg-amber-400 text-gray-700">
           25 Basic Projects while Learning React
         </h1>
       </section>
